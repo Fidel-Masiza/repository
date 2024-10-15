3b26dd16c55e6cb8c18f9a68be3831368cdf82eb
Masiza-Fidel <fidelmasitsa03@gmail.com>
2024-10-15 11:16:25 +0200

    mylawd

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import os
-from dotenv import load_dotenv
 import streamlit as st
 import requests
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Retrieve the Hugging Face API key from environment variables
-HUGGING_FACE_API_KEY = os.getenv("HUGGING_FACE_API_KEY")
 
 # Function to get responses from Hugging Face model
 def get_hugging_face_response(question):
     # Specify the model endpoint (you can change this to any supported model)
     API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large"
     
-    headers = {"Authorization": f"Bearer {HUGGING_FACE_API_KEY}"}
+    # Retrieve the Hugging Face API key from environment variables
+    hugging_face_api_key = os.getenv("HUGGING_FACE_API_KEY")
+    
+    headers = {"Authorization": f"Bearer {hugging_face_api_key}"}
     payload = {"inputs": question}
 
     response = requests.post(API_URL, headers=headers, json=payload)
